be8312399bc53ccae64e498e15d86bce33902b45
Christian Emmanuel <emmanuelchristian1990@gmail.com>
2022-09-09 18:59:26 -0700

    my task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - Prints a text
+ * main-entry point for the program
  *
- * Return: Always 0 (Success)
+ * Return:0 if no errors, return non zero if errors
  */
 int main(void)
 {
