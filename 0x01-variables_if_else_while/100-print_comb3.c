9a4eb588556680b5245ce0cfe5b5af8c580056ee
Christian Emmanuel <emmanuelchristian1990@gmail.com>
2022-09-10 06:17:46 -0700

    my task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,18 +7,19 @@
 
 int main(void)
 {
-	int a;
-	int b;
-	for (a = 0; a <= 9; a++)
-	{
-		for (b = 1; b <= 9; b++)
-		{
-			if (b > a)
-			{
-				putchar(a + '0');
-				putchar(b + '0');
-				if (a != 8)
-				{
-					putchar(',');
-					putchar(' ');
+int a;
+int b;
+for (a = 0; a <= 9; a++)
+{
+for (b = 1; b <= 9; b++)
+{
+if (b > a)
+{
+putchar(a + '0');
+putchar(b + '0');
+if (a != 8)
+{
+putchar(',');
+putchar(' ');
+
 				
