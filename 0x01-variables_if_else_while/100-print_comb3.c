bcf27e3b9e50398421881f9f01e2b1fcb074a6b5
Christian Emmanuel <emmanuelchristian1990@gmail.com>
2022-09-10 06:10:52 -0700

    task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - this is to print double figures
+ * Return: will be 0 as (success)
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 1; b <= 9; b++)
+		{
+			if (b > a)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				if (a != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				
