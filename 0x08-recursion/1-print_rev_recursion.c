f64dc59affe4159c60591e08031eb2616a31d185
Chrisababio <emmanuelchristian@gmail.com>
2022-09-28 20:04:02 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _input_recurseion - function
+ * @s: input
+ * Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
